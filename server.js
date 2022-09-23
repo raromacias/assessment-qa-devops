0e50dc35581b01d29e12719d118a072cd65d928a
Raro Macias <ggmacias@gmail.com>
2022-09-23 19:11:11 -0400

    addedtests

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/css', (req, res) => {
+app.get('/styles', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.css'))
 })
 
