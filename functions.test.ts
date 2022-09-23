250016b7702639a19fb0d642f866e6481e092f94
Raro Macias <ggmacias@gmail.com>
2022-09-23 14:44:48 -0400

    first

---
+++ functions.test.ts
@@ -0,0 +1,9 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    // CODE HERE
+    // test('matches if array', () => {
+    //     expect().toEqual(expect.arrayContaining(shuffleArray))
+    
+    //   })
+})
